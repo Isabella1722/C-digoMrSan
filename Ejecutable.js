28295018680ecd677af8c3e7123dc62f11fab8ab
Isabella1722 <isabellalinarescamacho0517@gmail.com>
2019-09-08 15:05:58 -0500

    Se añadieron los recursos

--- Ejecutable.js
+++ Ejecutable.js
@@ -1,15 +1,17 @@
 let pantalla;
+let inicio;
 function settings (){
 createCanvas (375,812);
     
 }
 function setup (){
-
+inicio=loadImage("Inicio.jpg");
 
 }
 
 function draw (){
-
+imageMode(CENTER);
+image(inicio, width/2,height/2,375,812);
     
 }
 function mousePressed (){
