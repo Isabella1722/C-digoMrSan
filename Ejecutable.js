02719b16246461c8ff42cc89703ec5f8dec31f17
Isabella1722 <isabellalinarescamacho0517@gmail.com>
2019-09-08 16:49:31 -0500

    Se cargaron las imágenes

--- Ejecutable.js
+++ Ejecutable.js
@@ -1,12 +1,34 @@
 
 let pantalla;
-let inicio;
+let inicio, acompa,acompaNumero,adiciones,alerta1,alerta2,cerrarSesion,confirmarDir,efectivo,pago,factura,
+historialPedido,menu,menuH,papitas,registro,sCriollo,sVegetariano,sQubano,sItaliano,tarjeta;
 let xd;
 let cas;
 let cap;
 
 function preload() {
     inicio = loadImage("data/inicio.jpg")
+    acompa = loadImage("data/Acompa.jpg")
+    acompaNumero = loadImage("data/AcompaNumero.jpg")
+    adiciones = loadImage("data/Adiciones.jpg")
+    alerta1 = loadImage("data/Alerta1.jpg")
+    alerta2 = loadImage("data/Alerta2.jpg")
+    cerrarSesion = loadImage("data/CerrarSesion.jpg")
+    confirmarDir = loadImage("data/ConfirmarDir.jpg")
+    efectivo = loadImage("data/Efectivo.jpg")
+    pago = loadImage("data/Pago.jpg")
+    factura = loadImage("data/Factura.jpg")
+    historialPedido = loadImage("data/HistorialPedido.jpg")
+    menu = loadImage("data/Menu.jpg")
+    menuH = loadImage("data/MenuHamburguesa.jpg")
+    papitas = loadImage("data/Papitas.jpg")
+    registro = loadImage("data/Registro.jpg")
+    sCriollo = loadImage("data/SCriollo.jpg")
+    sVegetariano = loadImage("data/SVegetariano.jpg")
+    sQubano = loadImage("data/SQubano.jpg")
+    sItaliano = loadImage("data/SItaliano.jpg")
+    tarjeta=loadImage("data/Tarjeta.jpg")
+
 }
 
 function setup() {
@@ -17,8 +39,73 @@ function setup() {
 }
 function draw() {
     background(0);
-    image(inicio, 0, 0);
+    
     switch (pantalla){
+ 
+
+        case 0:
+            //PANTALLA INICIO
+                image(inicio, 0, 0);
+
+
+
+        break;
+
+
+        case 1:
+
+           //PANTALLA REGISTRO
+           menu = loadImage("data/Menu.jpg")
+
+        break;
+
+
+        case 2:
+            
+           //PANTALLA MENU
+
+        break;
+
+        case 3:
+            
+           //PANTALLA MENU HAMBURGUES
+
+        break;
+
+        case 4:
+
+           //PANTALLA SANDWICH
+
+        break;
+
+
+        case 5:
+
+           //PANTALLA ALERTA ADICIONES
+        break;
+
+        case 6:
+
+           //PANTALLA ALERTA ACOMPAÑAMIENTOS
+        break;
+
+        case 7:
+
+           //PANTALLA PAGO
+        break;
+        case 8:
+
+            //PANTALLA FACTURA
+             break;
+         case 9:
+
+            //PANTALLA CERRAR SESION
+          break;
+
+        case 10:
+
+        //PANTALLA HISTORIAL PEDIDO
+          break;
 
 
     }
