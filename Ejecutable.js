30375dc7130a7d48099ca66fd4807632068d11e1
stban2703 <s.t.ban1401@gmail.com>
2019-09-08 16:15:27 -0500

    Modifiqué ejecutable

--- Ejecutable.js
+++ Ejecutable.js
@@ -22,11 +22,5 @@ function draw() {
 
 function mousePressed() {
 
-
-<<<<<<< HEAD
-
-=======
 }
-
 // alt shift f  para acomodar las lineas de código
->>>>>>> 29c671dfcc758a2a3b03eb0426ecb988ac057caa
