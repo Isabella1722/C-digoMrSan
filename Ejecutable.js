7ab6a19d3bc07771b7ae8e3eb21853a009498584
Leryru <valesibaja10@hotmail.com>
2019-09-09 21:43:11 -0500

    Se crean algunas funciones para cambio de pantallas (primera parte)

--- Ejecutable.js
+++ Ejecutable.js
@@ -1,7 +1,7 @@
 
 let pantalla;
 let inicio, acompa, acompaNumero, adiciones, alerta1, alerta2, cerrarSesion, confirmarDir, efectivo, pago, factura,
-    historialPedido, menu, menuH, papitas, registro, sCriollo, sVegetariano, sQubano, sItaliano, tarjeta;
+    historialPedido, menu, menuP, papitas, registro, sCriollo, sVegetariano, sQubano, sItaliano, tarjeta;
 
 function preload() {
     inicio = loadImage("data/inicio.jpg");
@@ -17,7 +17,7 @@ function preload() {
     factura = loadImage("data/Factura.jpg");
     historialPedido = loadImage("data/HistorialPedido.jpg");
     menu = loadImage("data/Menu.jpg");
-    menuH = loadImage("data/MenuHamburguesa.jpg");
+    menuP = loadImage("data/menuPerfil.jpg");
     papitas = loadImage("data/Papitas.jpg");
     registro = loadImage("data/Registro.jpg");
     sCriollo = loadImage("data/SCriollo.jpg");
@@ -38,57 +38,79 @@ function draw() {
     background(0);
     cursor(ARROW);
     switch (pantalla) {
-       
+
 
         case 0:
             //PANTALLA INICIO
             image(inicio, 0, 0);
             if (mouseX >= 90 && mouseX <= 282 && mouseY >= 543 && mouseY <= 589) {
                 cursor(HAND);
-               
-        
-        
             }
-
+            if (mouseX >= 124 && mouseX <= 246 && mouseY >= 623 && mouseY <= 646) {
+                cursor(HAND);
+            }
 
             break;
 
 
         case 1:
 
-            //PANTALLA Menu
+            //PANTALLA MENU
             image(menu, 0, 0);
-            
+            if (mouseX >= 0 && mouseX <= 67 && mouseY >= 1 && mouseY <= 66) {
+                cursor(HAND);
+            }
+            if (mouseX >= 34 && mouseX <= 343 && mouseY >= 336 && mouseY <= 417) {
+                cursor(HAND);
+            }
+            if (mouseX >= 34 && mouseX <= 343 && mouseY >= 456 && mouseY <= 536) {
+                cursor(HAND);
+            }
+            if (mouseX >= 34 && mouseX <= 343 && mouseY >= 570 && mouseY <= 652) {
+                cursor(HAND);
+            }
+
             break;
 
 
         case 2:
 
-            //PANTALLA Registro
+            //PANTALLA REGISTRO
             image(registro, 0, 0);
+            if (mouseX >= 0 && mouseX <= 67 && mouseY >= 1 && mouseY <= 66 && pantalla == 2) {
+                cursor(HAND);
+            }
+            if (mouseX >= 159 && mouseX <= 214 && mouseY >= 551 && mouseY <= 608) {
+                cursor(HAND);
+            }
+
             break;
 
         case 3:
 
-            //PANTALLA MENU HAMBURGUES
-            image(menuH, 0, 0);
+            //PANTALLA MENU PERFIL
+            image(menuP, 0, 0);
+
             break;
 
         case 4:
 
-            //PANTALLA SANDWICH
+            //PANTALLA SANDWICH CRIOLLO
+            image(sCriollo, 0, 0);
 
             break;
 
 
         case 5:
 
-            //PANTALLA ALERTA ADICIONES
+            //PANTALLA SANDWICH CUBANO
+            image(sQubano, 0, 0);
             break;
 
         case 6:
 
-            //PANTALLA ALERTA ACOMPAÑAMIENTOS
+             //PANTALLA SANDWICH CUBANO
+             image(sItaliano, 0, 0);
             break;
 
         case 7:
@@ -115,34 +137,42 @@ function draw() {
 }
 
 function mousePressed() {
-    print(mouseX + " " + mouseY + " \n");
+    print(mouseX + "X" + mouseY + "Y \n");
     //BOTÓN ENTRAR 
     if (mouseX >= 90 && mouseX <= 282 && mouseY >= 543 && mouseY <= 589 && pantalla == 0) {
-        cursor(HAND);
         pantalla = 1;
-
-
     }
-    //BOTÓN MENÚ HAMBURGUESA
-   if (mouseX >= 0 && mouseX <= 67 && mouseY >= 1 && mouseY <= 66 && pantalla == 1) {
-        cursor(HAND);
+    //BOTÓN MENÚ PERFIL
+    if (mouseX >= 0 && mouseX <= 67 && mouseY >= 1 && mouseY <= 66 && pantalla == 1) {
         pantalla = 3;
-
-
     }
-  // BOTÓN REGISTRO
-    if (mouseX >= 124 && mouseX <= 246 && mouseY >= 623 && mouseY <= 646 && pantalla == 0 ) {
-        cursor(HAND);
+    if (mouseX >= 265 && pantalla == 3) {
+        pantalla = 1;
+    }
+    // BOTÓN ENTRAR REGISTRO
+    if (mouseX >= 124 && mouseX <= 246 && mouseY >= 623 && mouseY <= 646 && pantalla == 0) {
         pantalla = 2;
-
-
     }
-  // BOTÓN REGISTRO VOLVER MENU
+    print(pantalla);
+    // BOTÓN REGISTRO VOLVER 
     if (mouseX >= 0 && mouseX <= 67 && mouseY >= 1 && mouseY <= 66 && pantalla == 2) {
-        cursor(HAND);
-        pantalla = 1 ;
-
-
+        pantalla = 0;
+    }
+    // BOTÓN REGISTRO
+    if (mouseX >= 159 && mouseX <= 214 && mouseY >= 551 && mouseY <= 608 && pantalla == 2) {
+        pantalla = 1;
+    }
+    //BOTÓN SANWICH CRIOLLO
+    if (mouseX >= 34 && mouseX <= 343 && mouseY >= 336 && mouseY <= 417 && pantalla == 1) {
+        pantalla = 4;
+    }
+    //BOTÓN SANWICH CUBANO
+    if (mouseX >= 34 && mouseX <= 343 && mouseY >= 456 && mouseY <= 536 && pantalla == 1) {
+        pantalla = 5;
+    }
+     //BOTÓN SANWICH ITALIANO
+     if (mouseX >= 34 && mouseX <= 343 && mouseY >= 570 && mouseY <= 652 && pantalla == 1) {
+        pantalla = 6;
     }
 
 }
